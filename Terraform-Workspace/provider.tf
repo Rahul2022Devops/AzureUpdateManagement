ad6107420177410e7c56dccc894ea1d896e7dd0a
Rahul Hasan <careerrahulhasan93@gmail.com>
2024-05-26 17:53:25 +0530

    Provider file updated for client secret

--- Terraform-Workspace/provider.tf
+++ Terraform-Workspace/provider.tf
@@ -8,8 +8,9 @@ terraform {
 }
 provider "azurerm" {
   features {}
+  
   subscription_id = "8a24fe43-dfae-40d8-a480-95972e119947"
   client_id       = "e235661c-87e1-4f78-a404-5982e85a7eca"
-  client_secret   = "57eaa259-e264-4f2e-b7e5-a1646f52f1da"
+  client_secret   = "sjz8Q~WWyyR6Tt5IC9XtweesSBMlGQO-B-GNbcwI"
   tenant_id       = "3f1ad11d-be94-448d-b44b-77fd3a8dbc97"
 }
